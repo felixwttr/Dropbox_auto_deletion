c390bb655a2a60aa09cb922516e1977a35e74aaa
Felix Wittwer <felix.wittwer99@icloud.com>
2021-07-19 15:11:56 +0200

    first commit

---
+++ Dropbox_auto_deletion.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import dropbox
+import os
+from datetime import datetime
+import pytz
+
+#Bitte anpassen
+TOKEN = 'YOUR-TOKEN-HERE'
+age_of_files = 30
+working_dir = '/Test/'
+
+
+now = datetime.now(pytz.utc).strftime('%Y-%m-%d %H:%M:%S')
+now = datetime.strptime(now, '%Y-%m-%d %H:%M:%S')
+
+#Verbindung zur Dropbox aufbauen
+dbx = dropbox.Dropbox(TOKEN)
+
+# Inhalt des Working Directorys auslesen
+content_to_return = []
+folder_content = dbx.files_list_folder(working_dir)
+while folder_content.has_more:
+    cursor = folder_content.cursor
+    folder_content = dropbox.dropbox.Dropbox.files_list_folder_continue(cursor)
+    dir_content += folder_contents.entries
+
+dir_content = folder_content.entries
+for fobj in dir_content:
+        if isinstance(fobj, dropbox.files.FileMetadata):
+            content_to_return.append(fobj.name)
+        else:
+            print('Datei konnte nicht der Liste der Dateien hinzugefügt werden')
+
+# Prüfen ob Datei älter als age_of_file Tage alt ist, falls ja löschen, falls nein nichts tun
+for file in content_to_return:
+    metainfo = dbx.files_get_metadata(working_dir + file).client_modified
+    created = datetime.strptime(str(metainfo), '%Y-%m-%d %H:%M:%S')
+    delta = (now - created).days
+    if delta >= age_of_files:
+        dbx.files_delete(working_dir + file)
+        print('Die Datei ' + str(file) + ' wurde gelöscht!')
+    else:
+        print('Die Datei ' + str(file) + ' ist noch keine ' + str(age_of_files) + ' Tage alt.')
+
+
